18ebfac69a590d41072966ff191623dd205b06d5
Fabio daniel <fdaniel.aborges@gmail.com>
2024-09-05 14:38:54 -0300

    ajustando controller

--- controller/todo_controller.js
+++ controller/todo_controller.js
@@ -2,13 +2,13 @@ import Todo from '../models/todo.js';
 
 const createTodo = async (req, res) => {
   try {
-    const  description  = req.body;
+    const { description } = req.body;
     const newTodo = Todo({description:description});
     await newTodo.save();
     res.json(newTodo);
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Erro ao criar o toDo.' });
+    res.status(500).json({ error: 'Erro ao criar o ToDo.' });
   }
 };
 
@@ -18,43 +18,42 @@ const getTodos = async (req, res) => {
     res.json(todos);
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Erro ao buscar os toDos.' });
+    res.status(500).json({ error: 'ToDo ao buscar os todos.' });
   }
 };
 
 const getTodoById = async (req, res) => {
   try {
-    const  id  = req.params;
+    const { id } = req.params;
     const todo = await Todo.findById(id);
-   
-      res.json(todo);
+    res.json(todo);
    
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Erro ao buscar o toDo.' });
+    res.status(500).json({ error: 'Erro ao buscar o ToDo.' });
   }
 };
 
 const updateTodo = async (req, res) => {
   try {
-    const  id  = req.params;
-    const  description  = req.body;
+    const { id } = req.params;
+    const { description } = req.body;
     const updatedTodo = await Todo.findByIdAndUpdate(id, { description:description }, { new: true });
     res.json(updatedTodo);
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Erro ao atualizar o toDo.' });
+    res.status(500).json({ error: 'Erro ao atualizar o ToDo.' });
   }
 };
 
 const deleteTodo = async (req, res) => {
   try {
-    const  id  = req.params;
+    const { id } = req.params;
     const deleted = await Todo.findByIdAndDelete(id);
     res.json({ success: deleted ? 'ToDo deletado.' : 'ToDo não encontrado.' });
   } catch (err) {
     console.log(err)
-    res.status(500).json({ error: 'Erro ao deletar o toDo.' });
+    res.status(500).json({ error: 'Erro ao deletar o ToDo.' });
   }
 };
 
